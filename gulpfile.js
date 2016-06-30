4a47b5b9fa82e1aafc74c23cf7fdac373df23a98
lxp <466849786@qq.com>
2016-06-30 15:52:33 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	runSequence = require('run-sequence'),
+	notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+	browserSync = require('browser-sync').create(),
+	reload = browserSync.reload;
+
+gulp.task('watch', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		},
+		port: 9100
+	});
+	gulp.watch('less/**/*.less', ['less']);
+	gulp.watch("examples/**/*.html").on('change', reload);
+});
+
+gulp.task('less', function() {
+	return gulp.src('./less/ui.less')
+		.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+		.pipe(less())
+		.pipe(gulp.dest('./dist/'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+gulp.task('default', function() {
+	runSequence(
+		'less',
+		'watch'
+	);
+});
